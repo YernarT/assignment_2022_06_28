bd3229273728e780766f8679cf9e2f836b2e3fcd
YernarT <toktaryernar@gmail.com>
2022-06-29 00:26:48 +0600

    feat: :sparkles: add container common style

---
+++ frontend/src/assets/style/container.ts
@@ -0,0 +1,44 @@
+import { css } from 'styled-components';
+
+type conatinerAttr = 'padding' | 'width';
+
+export default (attr: conatinerAttr = 'padding') => css`
+	${attr === 'padding'
+		? `
+	padding: 0 5rem;
+	`
+		: `
+	width: calc(100% - 5rem * 2);
+	`}
+	margin: auto;
+
+	@media screen and (max-width: 800px) {
+		${attr === 'padding'
+			? `
+	padding: 0 3rem;
+	`
+			: `
+	width: calc(100% - 3rem * 2);
+	`}
+	}
+
+	@media screen and (max-width: 600px) {
+		${attr === 'padding'
+			? `
+	padding: 0 0.15rem;
+	`
+			: `
+	width: calc(100% - 0.15rem * 2);
+	`}
+	}
+
+	@media screen and (max-width: 400px) {
+		${attr === 'padding'
+			? `
+	padding: 0 0.75rem;
+	`
+			: `
+	width: calc(100% - 0.75rem * 2);
+	`}
+	}
+`;
