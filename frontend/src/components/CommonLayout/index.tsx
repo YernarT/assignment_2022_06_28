ab57395461ff41da76821abbfe177bd0b45a35fe
YernarT <toktaryernar@gmail.com>
2022-06-29 12:41:10 +0600

    update: :hammer: commonLayout done

--- frontend/src/components/CommonLayout/index.tsx
+++ frontend/src/components/CommonLayout/index.tsx
@@ -1,3 +1,27 @@
+// React
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// 组件
+import Header from './Header';
+// 样式组件
+import { CommonLayoutStyled } from './style';
+
+// 懒加载页面
+const LandingPage = lazy(() => import('@/pages/LandingPage'));
+const ArticlePage = lazy(() => import('@/pages/ArticlePage'));
+
 export default function CommonLayout() {
-	return <div>CommonLayout</div>;
+	return (
+		<BrowserRouter>
+			<CommonLayoutStyled>
+				<Header />
+
+				<Suspense fallback={<div>Loading...</div>}>
+					<Route exact path="/" component={LandingPage} />
+					<Route exact path="/articles" component={ArticlePage} />
+				</Suspense>
+			</CommonLayoutStyled>
+		</BrowserRouter>
+	);
 }
---
+++ frontend/src/components/CommonLayout/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const CommonLayoutStyled = styled.div`
+	width: 100%;
+	height: 100%;
+
+	position: relative;
+	overflow: hidden;
+`;
