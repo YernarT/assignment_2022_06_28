132492c9594874e11b20061eec231d4caaca5c88
YernarT <toktaryernar@gmail.com>
2022-06-29 20:14:42 +0600

    update: :hammer: LandingPage done

--- frontend/src/pages/LandingPage/index.tsx
+++ frontend/src/pages/LandingPage/index.tsx
@@ -1,14 +1,12 @@
-// React
-import { useEffect, useRef } from 'react';
-
 // 组件
-import { Typography } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
+import { Typography, Result, Button } from 'antd';
+import { DownOutlined, SmileOutlined } from '@ant-design/icons';
 // 样式组件
 import {
 	LandingPageStyled,
 	IntroductionSectionStyled,
-	CardSectionStyled,
+	InterimSectionStyled,
+	FooterSectionStyled,
 } from './style';
 
 // 业务库
@@ -49,7 +47,114 @@ export default function LandingPage() {
 				/>
 			</IntroductionSectionStyled>
 
-			<CardSectionStyled></CardSectionStyled>
+			<InterimSectionStyled>
+				<Result
+					icon={<SmileOutlined />}
+					title={
+						<div className="sentence">
+							<Typography.Text>
+								I’ll call for pen and ink, and write my mind.
+							</Typography.Text>
+							<Typography.Text>
+								Moist it again, and frame some feeling line.
+							</Typography.Text>
+							<Typography.Text>
+								Write till your ink be dry, and with tears.
+							</Typography.Text>
+						</div>
+					}
+					extra={
+						<Button type="primary" className="next-btn">
+							Узнать болшее
+						</Button>
+					}
+				/>
+			</InterimSectionStyled>
+
+			<FooterSectionStyled>
+				<div className="gs-footerDataContainer">
+					<div className="gs-footerInnerContainer">
+						<div className="gs-about-us">
+							<Typography.Title level={3} className="title">
+								О нас
+							</Typography.Title>
+							<Typography.Paragraph>
+								Lorem Ipsum is simply dummy text of the printing and typesetting
+								industry. Lorem Ipsum has been the industry's standard dummy
+								text ever since the 1500s, when an unknown printer took a galley
+								of type and scrambled it to make a type specimen book.
+							</Typography.Paragraph>
+						</div>
+						<div className="gs-fotLinks">
+							<Typography.Text>Links group</Typography.Text>
+							<div className="gs-footBody">
+								<ul className="gs-links-list">
+									<li>
+										<a>Link 1</a>
+									</li>
+									<li>
+										<a>Link 2</a>
+									</li>
+									<li>
+										<a>Link 3</a>
+									</li>
+									<li>
+										<a>Link 4</a>
+									</li>
+									<li>
+										<a>Link 5</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div className="gs-fotLinks">
+							<Typography.Text>Links group</Typography.Text>
+							<div className="gs-footBody">
+								<ul className="gs-links-list">
+									<li>
+										<a>Link 1</a>
+									</li>
+									<li>
+										<a>Link 2</a>
+									</li>
+									<li>
+										<a>Link 3</a>
+									</li>
+									<li>
+										<a>Link 4</a>
+									</li>
+									<li>
+										<a>Link 5</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<hr className="gs-footerHr" />
+					<div className="gs-footerInnerContainer">
+						<div className="gs-fotter-left">
+							Copyright © 2022 <a>IT IS IT</a>.
+						</div>
+						<div className="gs-fotter-right">
+							<a className="gs-social-icon">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+									<path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+								</svg>
+							</a>
+							<a className="gs-social-icon">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+									<path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+								</svg>
+							</a>
+							<a className="gs-social-icon">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+									<path d="M24 11.779c0-1.459-1.192-2.645-2.657-2.645-.715 0-1.363.286-1.84.746-1.81-1.191-4.259-1.949-6.971-2.046l1.483-4.669 4.016.941-.006.058c0 1.193.975 2.163 2.174 2.163 1.198 0 2.172-.97 2.172-2.163s-.975-2.164-2.172-2.164c-.92 0-1.704.574-2.021 1.379l-4.329-1.015c-.189-.046-.381.063-.44.249l-1.654 5.207c-2.838.034-5.409.798-7.3 2.025-.474-.438-1.103-.712-1.799-.712-1.465 0-2.656 1.187-2.656 2.646 0 .97.533 1.811 1.317 2.271-.052.282-.086.567-.086.857 0 3.911 4.808 7.093 10.719 7.093s10.72-3.182 10.72-7.093c0-.274-.029-.544-.075-.81.832-.447 1.405-1.312 1.405-2.318zm-17.224 1.816c0-.868.71-1.575 1.582-1.575.872 0 1.581.707 1.581 1.575s-.709 1.574-1.581 1.574-1.582-.706-1.582-1.574zm9.061 4.669c-.797.793-2.048 1.179-3.824 1.179l-.013-.003-.013.003c-1.777 0-3.028-.386-3.824-1.179-.145-.144-.145-.379 0-.523.145-.145.381-.145.526 0 .65.647 1.729.961 3.298.961l.013.003.013-.003c1.569 0 2.648-.315 3.298-.962.145-.145.381-.144.526 0 .145.145.145.379 0 .524zm-.189-3.095c-.872 0-1.581-.706-1.581-1.574 0-.868.709-1.575 1.581-1.575s1.581.707 1.581 1.575-.709 1.574-1.581 1.574z" />
+								</svg>
+							</a>
+						</div>
+					</div>
+				</div>
+			</FooterSectionStyled>
 		</LandingPageStyled>
 	);
 }
--- frontend/src/pages/LandingPage/style.ts
+++ frontend/src/pages/LandingPage/style.ts
@@ -1,5 +1,7 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
+// 高阶组件
+import { Layout } from 'antd';
 // 公共样式
 import { container } from '@/assets/style';
 // 图片资源
@@ -17,6 +19,8 @@ export const IntroductionSectionStyled = styled.section<{
 	height: ${({ vh }) => (vh ? `${100 * vh}px` : '100vh')};
 	padding-top: calc(60px + 32px + 16px) !important;
 	padding-bottom: 16px !important;
+	box-shadow: 0 6px 16px -8px rgba(0, 0, 0, 0.8),
+		0 9px 28px 0px rgba(0, 0, 0, 0.5), 0 12px 48px 16px rgba(0, 0, 0, 0.3);
 
 	background-image: url(${LandingBackground});
 	background-size: cover;
@@ -59,5 +63,248 @@ export const IntroductionSectionStyled = styled.section<{
 		cursor: pointer;
 
 		${({ theme }) => theme.shape.iconAppearance.apply}
+	}
+`;
+
+export const InterimSectionStyled = styled.section`
+	${container()}
+	padding-top: 32px !important;
+	padding-bottom: 32px !important;
+
+	.sentence {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.next-btn {
+		box-sizing: content-box;
+		padding: 16px 40px;
+
+		font-size: 1.28rem;
+	}
+`;
+
+export const FooterSectionStyled = styled(Layout.Footer)`
+	.gs-footerDataContainer {
+		padding: var(--footerPadding-gs-footer-one);
+		height: 100%;
+	}
+
+	.gs-footerDataContainer .gs-footerInnerContainer {
+		display: flex;
+		justify-content: space-around;
+	}
+
+	.gs-footerDataContainer .gs-footerInnerContainer .gs-about-us {
+		width: 50%;
+		height: 100%;
+		padding: 10px;
+	}
+
+	.gs-footerDataContainer .gs-footerInnerContainer .gs-fotLinks {
+		width: 25%;
+		padding: 10px;
+	}
+
+	.gs-footerDataContainer .gs-footerInnerContainer {
+		padding-bottom: 10px;
+	}
+
+	.gs-footerDataContainer .gs-footerInnerContainer .gs-footBody {
+		color: var(--footerBody-Text-Color);
+	}
+
+	.gs-footerDataContainer .gs-footerInnerContainer .gs-footBody .gs-links-list {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	.gs-footerDataContainer
+		.gs-footerInnerContainer
+		.gs-footBody
+		.gs-links-list
+		li {
+		padding: 0px 0px 8px 0px;
+		width: max-content;
+	}
+
+	.gs-footerDataContainer
+		.gs-footerInnerContainer
+		.gs-footBody
+		.gs-links-list
+		li
+		a {
+		cursor: pointer;
+		display: block;
+		transition: var(--transition-time-gs-footer-one);
+	}
+
+	.gs-footerDataContainer
+		.gs-footerInnerContainer
+		.gs-footBody
+		.gs-links-list
+		li:hover
+		a {
+	}
+
+	.gs-footerDataContainer .gs-footerHr {
+		margin: 20px 0px;
+	}
+
+	.gs-footerDataContainer .gs-footerInnerContainer .gs-fotter-left {
+		width: 50%;
+		text-align: left;
+	}
+
+	.gs-footerDataContainer .gs-footerInnerContainer .gs-fotter-left a {
+		display: inline-block;
+	}
+
+	.gs-footerDataContainer .gs-footerInnerContainer .gs-fotter-left a:hover {
+		cursor: pointer;
+	}
+
+	.gs-footerDataContainer .gs-footerInnerContainer .gs-fotter-right {
+		width: 50%;
+		justify-content: right;
+		display: flex;
+	}
+
+	.gs-footerDataContainer
+		.gs-footerInnerContainer
+		.gs-fotter-right
+		.gs-social-icon {
+		border-radius: 50%;
+		background-color: #818a91;
+		cursor: pointer;
+		height: 45px;
+		width: 45px;
+		margin-left: 10px;
+		position: relative;
+		transition: var(--transition-time-gs-footer-one);
+		display: block;
+	}
+
+	.gs-footerDataContainer
+		.gs-footerInnerContainer
+		.gs-fotter-right
+		.gs-social-icon
+		svg {
+		height: 30px;
+		position: absolute;
+		margin: auto;
+		top: 0;
+		left: 0;
+		bottom: 0;
+		right: 0;
+		fill: black;
+	}
+
+	.gs-footerDataContainer
+		.gs-footerInnerContainer
+		.gs-fotter-right
+		.gs-social-icon:hover {
+	}
+
+	@media screen and (max-width: 2560px) {
+		padding: 0px 20%;
+	}
+
+	@media screen and (max-width: 1440px) {
+		padding: 0px 10%;
+	}
+
+	@media screen and (max-width: 1024px) {
+		padding: 0px 5%;
+	}
+
+	@media screen and (max-width: 768px) {
+		padding: 0px 0px;
+	}
+
+	@media screen and (max-width: 520px) {
+		padding: 0px 0px;
+
+		.gs-footerDataContainer .gs-footerInnerContainer .gs-about-us {
+			width: 100%;
+			padding: 0px;
+		}
+
+		.gs-footerDataContainer .gs-footerInnerContainer .gs-fotLinks {
+			width: 50%;
+			padding: 0px;
+			padding-top: 20px;
+		}
+
+		.gs-footerDataContainer .gs-footerInnerContainer {
+			justify-content: space-between;
+			flex-wrap: wrap;
+		}
+
+		.gs-footerDataContainer .gs-footerInnerContainer .gs-fotter-left {
+			width: 100%;
+			text-align: center;
+		}
+
+		.gs-footerDataContainer .gs-footerInnerContainer .gs-fotter-right {
+			width: 100%;
+			padding-top: 20px;
+			justify-content: center;
+		}
+	}
+
+	@media screen and (max-width: 300px) {
+		padding: 0px 0px;
+
+		.gs-footerDataContainer .gs-footerInnerContainer .gs-fotLinks {
+			width: 100%;
+			padding: 0px;
+			padding-top: 20px;
+			text-align: center;
+		}
+
+		.gs-footerDataContainer
+			.gs-footerInnerContainer
+			.gs-footBody
+			.gs-links-list
+			li {
+			padding: 0px 0px 8px 0px;
+			width: 100%;
+		}
+
+		.gs-footerDataContainer
+			.gs-footerInnerContainer
+			.gs-fotter-right
+			.gs-social-icon {
+			width: 30px;
+			height: 30px;
+		}
+
+		.gs-footerDataContainer
+			.gs-footerInnerContainer
+			.gs-fotter-right
+			.gs-social-icon
+			svg {
+			height: 20px;
+		}
+	}
+
+	@media screen and (max-width: 230px) {
+		.gs-footerDataContainer
+			.gs-footerInnerContainer
+			.gs-fotter-right
+			.gs-social-icon {
+			width: 25px;
+			height: 25px;
+		}
+
+		.gs-footerDataContainer
+			.gs-footerInnerContainer
+			.gs-fotter-right
+			.gs-social-icon
+			svg {
+			height: 15px;
+		}
 	}
 `;
