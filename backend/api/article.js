54dfbb3138cd6212ed694acb3624c7baad2dd6b4
YernarT <toktaryernar@gmail.com>
2022-06-30 17:15:34 +0600

    feat: :sparkles: put method done

--- backend/api/article.js
+++ backend/api/article.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose');
 const ArticleModel = require('../db/schema');
 
 // Read
@@ -31,7 +32,27 @@ router.post('/articles', async (req, res) => {
 });
 
 // Update
-router.put('/articles', async (req, res) => {});
+router.put('/articles/:id', async (req, res) => {
+	let articleId = req.params.id;
+	let article;
+
+	try {
+		article = await ArticleModel.findById(articleId);
+	} catch (err) {
+		res.status(400).json({ 'message': 'Ресурс не существует' });
+	}
+
+	if (article) {
+		article.title = req.body.title ?? article.title;
+		article.content = req.body.content ?? article.content;
+
+		await article.save();
+
+		res.status(200).json(article);
+	} else {
+		res.status(400).json({ 'message': 'Ресурс не существует' });
+	}
+});
 
 // Delete
 router.delete('/articles', async (req, res) => {});
