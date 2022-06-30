b819b8749b6aca19ac4aa9d3735fcb30d4b0ea71
YernarT <toktaryernar@gmail.com>
2022-06-30 23:04:41 +0600

    feat: :sparkles: articles create, read methods

---
+++ frontend/src/service/api/article-api.ts
@@ -0,0 +1,40 @@
+// 类型
+import type { AxiosResponse } from 'axios';
+
+import { apiServerInstance } from '../ajax';
+
+export interface ArticleType4Create {
+	title: string;
+	content: string;
+}
+
+export interface ArticleType4Read {
+	title: string;
+	content: string;
+}
+
+export interface ErrorResponseType {
+	message: string;
+}
+
+// Create
+export const reqCreateArticle = (
+	data: ArticleType4Create,
+): Promise<AxiosResponse<ArticleType4Read, ErrorResponseType>> =>
+	apiServerInstance.post('/api/articles/', data);
+
+// Read
+export const reqGetArticles = (): Promise<
+	AxiosResponse<ArticleType4Read[], ErrorResponseType>
+> => apiServerInstance.get('/api/articles/');
+
+// Update
+export const reqUpdateArticle = (
+	data: ArticleType4Create,
+): Promise<AxiosResponse<ArticleType4Read, ErrorResponseType>> =>
+	apiServerInstance.put('/api/articles/', data);
+
+// Delete
+export const reqRemoveArticle = (): Promise<
+	AxiosResponse<{}, ErrorResponseType>
+> => apiServerInstance.delete('/api/articles/');
