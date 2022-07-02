facb827f3cca2fe6521f75ff026f9681bfb2b73b
YernarT <toktaryernar@gmail.com>
2022-07-02 23:54:12 +0600

    fix: :bug: 启动文件改名

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const server = express();
+const app = express();
 
-server
+app
 	.use(cors())
 	.use(bodyParser.urlencoded({ extended: false }))
 	.use(bodyParser.json());
 
-server.use('/api', require('./api/article'));
+app.use('/api', require('./api/article'));
 
-module.exports = server;
+app.listen(process.env.PORT || 8000, () =>
+	console.log('app is running on port 8000'),
+);
--- backend/index.js
+++
@@ -1,3 +0,0 @@
-const server = require('./server');
-
-server.listen(8000, () => console.log('Server is running on port 8000'));
